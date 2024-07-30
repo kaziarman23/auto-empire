e657885cc39b6b56453c009dc57724ac5c6d16aa
Ethernal Knight <kaziarman@proton.me>
2024-07-30 12:30:47 +0600

    data is being ready

---
+++ app/constants/index.js
@@ -0,0 +1,55 @@
+import HomePageImageOne from "../../public/images/homePageImageOne";
+import HomePageImageTwo from "../../public/images/homePageImageTwo";
+import HomePageImageThree from "../../public/images/homePageImageThree";
+import HomePageImageFour from "../../public/images/homePageImageFour";
+import MercedesBenzSClass from "../../public/images/Mercedes-Benz-S-Class";
+import PorscheGT3 from "../../public/images/PorscheGT3";
+import HomePageFooterImage from "../../public/images/homePageFooterImage";
+
+export const heroComponent = [
+  {
+    image: HomePageImageOne,
+    title: "Welcome to Auto Impire!",
+    discription:
+      "We're thrilled to have you here at Auto Impire, your premier destination for all things automotive. Whether you're looking for expert advice, the latest industry trends, or top-notch services to keep your vehicle in peak condition, you've come to the right place.",
+  },
+];
+
+export const latestComponent = [
+  {
+    imageOne: HomePageImageTwo,
+    imageTwo: HomePageImageThree,
+    imageThree: HomePageImageFour,
+    title: "Discover Our Latest Arrivals",
+    discription:
+      "At Auto Impire, we’re excited to present three of the most captivating vehicles that are making waves in the automotive world. Explore the epitome of luxury, the thrill of racing, and the power of SUVs with our latest selections",
+  },
+];
+
+export const topSellComponent = [
+  {
+    imageOne: MercedesBenzSClass,
+    imageTwo: PorscheGT3.jpg,
+    title: "Explore Our Top-Selling Vehicles",
+    discription:
+      "At Auto Impire, we’re proud to showcase our top-selling cars, each representing the pinnacle of excellence in their respective categories. Discover why these models have captured the hearts of our customers and continue to lead the way in luxury and performance",
+    modelOne: "Mercedes Benz S Class",
+    modelTwo: "Porsche 911 GT3",
+    modelDetailsOne:
+      "The S-Class has consistently been a benchmark for luxury sedans, combining advanced technology, superior comfort, and a prestigious brand reputation.",
+    modelDetailsTwo:
+      "Known for its exceptional track performance, precision engineering, and iconic design, the 911 GT3 has been a top choice among racing enthusiasts and performance car lovers.",
+  },
+];
+
+export const homePageLastImage = HomePageFooterImage;
+
+export const footerComponent = "Thank you for choosing Auto Impire. We look forward to serving you and making your automotive journey a smooth and enjoyable ride! Drive safely and enjoy the road ahead!"
+
+export const carsPageDiscription = {
+  Luxury:
+    "1. Luxury Redefined:[Luxury Car Model] Step into the lap of luxury with the [Luxury Car Model]. This vehicle combines elegance and comfort with cutting-edge technology and unparalleled craftsmanship. Experience a ride that transcends ordinary expectations and indulge in a new standard of sophistication.",
+  Racing:
+    "2. Unleash the Speed: [Racing Car Model]Feel the adrenaline rush with the [Racing Car Model]. Engineered for performance and built for speed, this racing marvel delivers exhilarating power and precision. Designed for those who crave the thrill of the track, it’s where engineering excellence meets high-octane excitement.",
+  SUV: "3. Versatility Meets Power: [SUV Car Model]Conquer any terrain with the [SUV Car Model]. This versatile SUV offers robust performance, advanced safety features, and spacious comfort for all your adventures. Whether you’re navigating city streets or off-road trails, it’s the perfect companion for every journey.",
+};
